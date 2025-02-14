b20eddf1559e6ce8adf09b26ba91a1c9a2e8e88a
Chaaben Amine <mohamedamine.benchaabene@esen.tn>
2025-02-14 15:17:23 +0100

    lastupdate

---
+++ register.php
@@ -0,0 +1,51 @@
+<?php 
+
+include 'includes/db.php';
+
+if(isset($_POST['signUp'])){
+    $FirstName=$_POST['fName'];
+    $LastName=$_POST['lName'];
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $password=md5($password);
+    $PhoneNumber=$_POST['phone'];
+
+     $checkEmail="SELECT * From users where email='$email'";
+     $result=$conn->query($checkEmail);
+     if($result->num_rows>0){
+        echo "Email Address Already Exists !";
+     }
+     else{
+        $insertQuery="INSERT INTO users(FirstName,LastName,email,password,PhoneNumber)
+                       VALUES ('$FirstName','$LastName','$email','$password','$PhoneNumber')";
+            if($conn->query($insertQuery)==TRUE){
+                header("location: index.php");
+            }
+            else{
+                echo "Error:".$conn->error;
+            }
+     }
+   
+
+}
+
+if(isset($_POST['signIn'])){
+   $email=$_POST['email'];
+   $password=$_POST['password'];
+   $password=md5($password) ;
+   
+   $sql="SELECT * FROM users WHERE email='$email' and password='$password'";
+   $result=$conn->query($sql);
+   if($result->num_rows>0){
+    session_start();
+    $row=$result->fetch_assoc();
+    $_SESSION['email']=$row['email'];
+    header("Location: homepage.php");
+    exit();
+   }
+   else{
+    echo "Not Found, Incorrect Email or Password";
+   }
+
+}
+?>
