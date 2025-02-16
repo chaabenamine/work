ce26082ba5d4af8f5d994e83e69a3ea5b3afc463
Chaaben Amine <mohamedamine.benchaabene@esen.tn>
2025-02-16 17:19:36 +0100

    update

--- crud/update.php
+++ crud/update.php
@@ -35,7 +35,7 @@ if(isset($_POST['submit'])){
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" >
+    <link href="../assets/style2.css" rel="stylesheet" >
     <title>crud operation</title>
   </head>
   <body>
